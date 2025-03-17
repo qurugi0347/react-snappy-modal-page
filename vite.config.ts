cc4f33d1599b6a0b8668ef22a87907d61e3fb64c
Jun <qurugi0347@gmail.com>
2025-03-17 22:53:17 +0900

    CHORE: 미사용 코드 삭제

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import linaria from '@linaria/vite'
 import wyw from '@wyw-in-js/vite';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), wyw()],
-  server: {
-    allowedHosts: ['8194-58-72-40-45.ngrok-free.app', 'localhost']
-  }
 })
